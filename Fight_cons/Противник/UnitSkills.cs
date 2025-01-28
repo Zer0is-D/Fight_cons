39059190125dc9c7540794bd10bc5741ec28bf13
zer0is@mail.ru <zer0is@mail.ru>
2025-01-28 20:16:14 +0300

    .

--- Fight_cons/Противник/UnitSkills.cs
+++ Fight_cons/Противник/UnitSkills.cs
@@ -193,7 +193,7 @@ namespace Fight_cons
             Output.NameAndId(victim);
 
             if (damag > attacker.TotalAttack)
-                Output.WriteColorLine(ConsoleColor.Yellow, "критические ", $"{damag} ", "урона! У ");
+                Output.WriteColorLine(ConsoleColor.Yellow, "критические  ", $"{damag} ", "урона! У ");
             else
                 Output.WriteColorLine(ConsoleColor.Yellow, "", $"{damag} ", "урона у ");
 
